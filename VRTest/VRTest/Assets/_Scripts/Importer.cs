a2ddf98896427204b5fac69c102f54f80f0ac501
Graham <graham.knopp@gmail.com>
2018-11-19 14:42:58 -0500

    visualizer scene and importer for both animations

--- VRTest/VRTest/Assets/_Scripts/Importer.cs
+++ VRTest/VRTest/Assets/_Scripts/Importer.cs
@@ -1,73 +1,159 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class Importer : MonoBehaviour {
 
-    public AnimationClip animation;
-    public TextAsset text;
+    public AnimationClip animationOVR;
+    public AnimationClip animationUnity;
+
+    public TextAsset DataOVR;
+    public TextAsset DataUnity;
+
     public bool read;
 
     public float positionFactor = 1;
     public int numLines;
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void Start()
+    {
+        if (GetComponent<Animation>())
+            GetComponent<Animation>().Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (read)
         {
             read = false;
 
-            animation.ClearCurves();
-            //EditorCurveBinding[] curveBinding = AnimationUtility.GetCurveBindings(animation);
-            //string s = curveBinding[0].propertyName;
-            AnimationCurve positionX = new AnimationCurve();
-            AnimationCurve positionY = new AnimationCurve();
-            AnimationCurve positionZ = new AnimationCurve();
-            AnimationCurve rotationX = new AnimationCurve();
-            AnimationCurve rotationY = new AnimationCurve();
-            AnimationCurve rotationZ = new AnimationCurve();
-            AnimationCurve rotationW = new AnimationCurve();
+            animationOVR.ClearCurves();
+            animationUnity.ClearCurves();
 
 
-            string[] lines = text.text.Split('\n');
-            string[] line = new string[9];
+            AnimationCurve positionXUnity = new AnimationCurve();
+            AnimationCurve positionYUnity = new AnimationCurve();
+            AnimationCurve positionZUnity = new AnimationCurve();
+            AnimationCurve rotationXUnity = new AnimationCurve();
+            AnimationCurve rotationYUnity = new AnimationCurve();
+            AnimationCurve rotationZUnity = new AnimationCurve();
+            AnimationCurve rotationWUnity = new AnimationCurve();
 
-            int numLines2 = Mathf.Min(numLines, lines.Length - 2);
 
-            float firstTimestamp = float.Parse(lines[1].Split(new char[] { ',' }, 9)[0]);
-            float lastTimestamp = float.Parse(lines[numLines2-1].Split(new char[] { ',' }, 9)[0]);
+            string[] linesOVR = DataOVR.text.Split('\n');
+            string[] linesUnity = DataUnity.text.Split('\n');
+
+
+            //ovr data has empty line at start and end
+            long firstTimestampOVR = long.Parse(linesOVR[1].Split(new char[] { ',' }, 9)[0]);
+            long lastTimestampOVR = long.Parse(linesOVR[linesOVR.Length - 2].Split(new char[] { ',' }, 9)[0]);
+
+
+            long firstTimestampUnity = long.Parse(linesUnity[0].Split(new char[] { ',' }, 9)[0]);
+            long lastTimestampUnity = long.Parse(linesUnity[linesUnity.Length - 2].Split(new char[] { ',' }, 9)[0]);
+
+
+            string[] lineUnity = new string[9];
+            string[] lineOVR = new string[9];
+
+            int firstIndexOVR = 1;
+            int firstIndexUnity = 0;
+
+            int lastIndexOVR = linesOVR.Length - 2;
+            int lastIndexUnity = linesUnity.Length - 2;
             
-            for (int i = 1; i < numLines2 -1; i++)
+            //find the index of the first common timestamp
+            while (firstIndexOVR < linesOVR.Length && firstIndexUnity < linesUnity.Length)
             {
+                lineOVR = linesOVR[firstIndexOVR].Split(new char[] { ',' }, 9);
+                lineUnity = linesUnity[firstIndexUnity].Split(new char[] { ',' }, 9);
 
-                line = lines[i].Split(new char[] { ',' }, 9);
-
-                if (line[1] == " HMD")
+                if (ulong.Parse(lineOVR[0]) < ulong.Parse(lineUnity[0]))
+                {
+                    firstIndexOVR++;
                     continue;
-
-                float timestamp = (float.Parse(line[0]) - firstTimestamp) / 1000f;
-                
-                positionX.AddKey(timestamp, float.Parse(line[2].Substring(5)) * positionFactor);
-                positionY.AddKey(timestamp, float.Parse(line[3].Substring(5)) * positionFactor);
-                positionZ.AddKey(timestamp, float.Parse(line[4].Substring(5)) * positionFactor);
-                
-                rotationX.AddKey(timestamp, float.Parse(line[5].Substring(6)) * positionFactor);
-                rotationY.AddKey(timestamp, float.Parse(line[6].Substring(6)) * positionFactor);
-                rotationZ.AddKey(timestamp, float.Parse(line[7].Substring(6)) * positionFactor);
-                rotationW.AddKey(timestamp, float.Parse(line[8].Substring(6)) * positionFactor);
+                }
+                else if (ulong.Parse(lineOVR[0]) > ulong.Parse(lineUnity[0]))
+                {
+                    firstIndexUnity++;
+                    continue;
+                }
+                else
+                    break;
             }
 
-            animation.SetCurve("", typeof(Transform), "m_LocalPosition.x", positionX);
-            animation.SetCurve("", typeof(Transform), "m_LocalPosition.y", positionY);
-            animation.SetCurve("", typeof(Transform), "m_LocalPosition.z", positionZ);
-            animation.SetCurve("", typeof(Transform), "m_LocalRotation.x", rotationX);
-            animation.SetCurve("", typeof(Transform), "m_LocalRotation.y", rotationY);
-            animation.SetCurve("", typeof(Transform), "m_LocalRotation.z", rotationZ);
-            animation.SetCurve("", typeof(Transform), "m_LocalRotation.w", rotationW);
+
+            //find the index of the last common timestamp
+            while (lastIndexOVR > 0 && lastIndexUnity > 0)
+            {
+                lineOVR = linesOVR[lastIndexOVR].Split(new char[] { ',' }, 9);
+                lineUnity = linesUnity[lastIndexUnity].Split(new char[] { ',' }, 9);
+
+                if (ulong.Parse(lineOVR[0]) > ulong.Parse(lineUnity[0]))
+                {
+                    lastIndexOVR--;
+                    continue;
+                }
+                else if (ulong.Parse(lineOVR[0]) < ulong.Parse(lineUnity[0]))
+                {
+                    lastIndexUnity--;
+                    continue;
+                }
+                else
+                    break;
+            }
+
+
+            ParseData(firstIndexOVR, lastIndexOVR, linesOVR, animationOVR);
+            ParseData(firstIndexUnity, lastIndexUnity, linesUnity, animationUnity);
+
+
+
         }
 
+    }
+
+    void ParseData(int firstLineIndex, int lastLineIndex, string[] data, AnimationClip animation)
+    {
+
+        AnimationCurve positionX = new AnimationCurve();
+        AnimationCurve positionY = new AnimationCurve();
+        AnimationCurve positionZ = new AnimationCurve();
+        AnimationCurve rotationX = new AnimationCurve();
+        AnimationCurve rotationY = new AnimationCurve();
+        AnimationCurve rotationZ = new AnimationCurve();
+        AnimationCurve rotationW = new AnimationCurve();
+
+
+        string[] line = new string[9];
+
+        long firstTimestamp = long.Parse(data[firstLineIndex].Trim().Split(new char[] { ',' }, 9)[0]);
+
+        for (int i = firstLineIndex; i < lastLineIndex; i++)
+        {
+            line = data[i].Trim().Split(new char[] { ',' }, 9);
+            float timestamp = (long.Parse(line[0]) - firstTimestamp) / 1000f;
+
+            positionX.AddKey(timestamp, float.Parse(line[2].Substring(4)) * positionFactor);
+            positionY.AddKey(timestamp, float.Parse(line[3].Substring(4)) * positionFactor);
+            positionZ.AddKey(timestamp, float.Parse(line[4].Substring(4)) * positionFactor);
+
+            rotationX.AddKey(timestamp, float.Parse(line[5].Substring(5)) * positionFactor);
+            rotationY.AddKey(timestamp, float.Parse(line[6].Substring(5)) * positionFactor);
+            rotationZ.AddKey(timestamp, float.Parse(line[7].Substring(5)) * positionFactor);
+            rotationW.AddKey(timestamp, float.Parse(line[8].Substring(5)) * positionFactor);
+        }
+
+        animation.SetCurve("", typeof(Transform), "m_LocalPosition.x", positionX);
+        animation.SetCurve("", typeof(Transform), "m_LocalPosition.y", positionY);
+        animation.SetCurve("", typeof(Transform), "m_LocalPosition.z", positionZ);
+        animation.SetCurve("", typeof(Transform), "m_LocalRotation.x", rotationX);
+        animation.SetCurve("", typeof(Transform), "m_LocalRotation.y", rotationY);
+        animation.SetCurve("", typeof(Transform), "m_LocalRotation.z", rotationZ);
+        animation.SetCurve("", typeof(Transform), "m_LocalRotation.w", rotationW);
     }
 
     public static float RemapUnclamped(float s, float from1, float from2, float to1, float to2)
